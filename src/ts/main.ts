520b20921d4c02d04bb58aa2f1c0bb76efb51629
Daniel Arzanipour <daniel.arzanipour@gmail.com>
2022-11-26 19:23:34 -0500

    make small edit

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,6 +1,8 @@
 import { getExpenses } from './utils/fetch';
 
-const days = document.querySelectorAll('[data-day]');
+const days = document.querySelectorAll(
+  '[data-day]'
+) as NodeListOf<HTMLTableCellElement>;
 
 window.document.addEventListener('DOMContentLoaded', () => {
   getExpenses('src/data/data.json').then((data) => {
@@ -8,9 +10,12 @@ window.document.addEventListener('DOMContentLoaded', () => {
       const barElement = days[i];
       const barValue = days[i].firstElementChild;
 
-      if (barElement instanceof HTMLTableCellElement) {
+      if (days.length > 0) {
         barElement.style.height = data[i].amount * 5 + 'px';
         barElement.dataset.tooltipValue = '$' + data[i].amount;
+      } else {
+        barElement.style.height = '10px';
+        barElement.dataset.tooltipValue = '$0';
       }
 
       if (barValue != null) {
